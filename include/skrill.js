09977e4ab9a6cbafaf50603428c56124082df79a
radogado <rado@me.com>
2014-02-14 11:31:26 +0200

    back-to-top with native JS

--- include/skrill.js
+++ include/skrill.js
@@ -254,6 +254,60 @@ function relay_parameters () {
 
 }
 
+// easing functions http://goo.gl/5HLl8
+Math.easeInOutQuad = function (t, b, c, d) {
+  t /= d/2;
+  if (t < 1) {
+    return c/2*t*t + b
+  }
+  t--;
+  return -c/2 * (t*(t-2) - 1) + b;
+};
+
+Math.easeInCubic = function(t, b, c, d) {
+  var tc = (t/=d)*t*t;
+  return b+c*(tc);
+};
+
+Math.inOutQuintic = function(t, b, c, d) {
+  var ts = (t/=d)*t,
+  tc = ts*t;
+  return b+c*(6*tc*ts + -15*ts*ts + 10*tc);
+};
+
+// requestAnimationFrame for Smart Animating http://goo.gl/sx5sts
+var requestAnimFrame = (function(){
+  return  window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function( callback ){ window.setTimeout(callback, 1000 / 60); };
+})();
+
+function scrollTo(to, callback, duration) {
+  // figure out if this is moz || IE because they use documentElement
+  var doc = (navigator.userAgent.indexOf('Firefox') != -1 || navigator.userAgent.indexOf('MSIE') != -1) ? document.documentElement : document.body,
+  start = doc.scrollTop,
+  change = to - start,
+  currentTime = 0,
+  increment = 20;
+  duration = (typeof(duration) === 'undefined') ? 500: duration;
+  var animateScroll = function(){
+    // increment the time
+    currentTime += increment;
+    // find the value with the quadratic in-out easing function
+    var val = Math.easeInOutQuad(currentTime, start, change, duration);
+    // move the document.body
+    doc.scrollTop = val;
+    // do the animation unless its over
+    if(currentTime < duration) {
+      requestAnimFrame(animateScroll);
+    } else {
+      if (callback && typeof(callback) === 'function') {
+        // the animation is done so lets callback
+        callback();
+      }
+    }
+  };
+  animateScroll();
+}
+
 /* ███████████████████ After DOM is created ███████████████████ */
 
 $(document).ready(function() {
@@ -372,29 +426,14 @@ $(document).ready(function() {
 	
 /* Add 'Back to top' button */
 
-	$('body').append('<a class="backtotop"> ⬆ </a>');
-	$('.backtotop').click ( function () {
-		$('body,html').animate( { scrollTop: 0 }, 400 );
-	});
+	var el = document.createElement('a');
+	el.innerHTML = ' ⬆ ';
+	el.onclick = function(){ scrollTo(0); return false; };
+	el.classList.add('backtotop');
+	document.body.appendChild(el);
 
-/* Hide Mobile Safari head */
-
-	if ( is_touch_device() ) {
-		
-		  // Set a timeout...
-	  setTimeout(function(){
-	    // Hide the address bar!
-	    window.scrollTo(0, 1);
-	  }, 0);
-	}
+/* Auto textarea height */
    	
-   	// Activate date picker
-/*
-	$( '[type=date], .datepicker' ).pickadate({
-		formatSubmit: 'yyyy/mm/dd'
-	})
-*/
-
 	$('textarea').keyup ( function () {
 		textArea = this;
 		while (
