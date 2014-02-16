020799de8f1ebde3407b4830d5dec3caa1ba3b00
Radoslav Sharapanov <rado@me.com>
2014-02-16 19:11:30 +0200

    removed jQ retina, prizes slider

--- include/skrill.js
+++ include/skrill.js
@@ -2,11 +2,6 @@ function is_touch_device() {
   return !!('ontouchstart' in window);
 }
 
-/*! jQuery Retina Plugin - v1.0 - 3/25/2012
-* https://github.com/tylercraft/jQuery-Retina
-* Copyright (c) 2012 Tyler Craft; Licensed MIT, GPL */
-(function(a){a.fn.retina=function(b){var c={dataRetina:!0,suffix:"",checkIfImageExists:!1,customFileNameCallback:"",overridePixelRation:!1};b&&jQuery.extend(c,b);var d=!1;if(c.overridePixelRation||window.devicePixelRatio>=2)d=!0;return this.each(function(){var b=a(this);b.addClass("retina-off");if(!d)return!1;var e="";c.dataRetina&&b.attr("data-retina")&&(e=b.attr("data-retina")),c.suffix&&(e||(e=b.attr("src")));if(c.suffix){var f=e.replace(/.[^.]+$/,""),g=e.replace(/^.*\./,"");e=f+c.suffix+"."+g}c.customFileNameCallback&&(e=c.customFileNameCallback(b)),c.checkIfImageExists&&e?a.ajax({url:e,type:"HEAD",success:function(){b.attr("src",e),b.removeClass("retina-off"),b.addClass("retina-on")}}):e&&(b.attr("src",e),b.removeClass("retina-off"),b.addClass("retina-on"))})}})(jQuery)
-
 /* URI parameters */
 
 function GetURLParameter( name, source ) // if 'source' is missing, get it from the URI.
@@ -85,60 +80,6 @@ function getURLParameters () { // return all URL parameters in an array
 	}
 	
 	return p;
-}
-
-/* Prizes slider: Bind control event handlers */
-
-var numberprizes = 0;
-var prizeswidth = 0;
-
-function picks(pick,tot) {
-	var ary = new Array(tot);
-	for (var i = 0; i < tot; i++) ary[i] = i+1;
-	function randOrd(){return (Math.round(Math.random())-0.5); }
-	ary.sort(randOrd);
-	return ary.slice(0,pick);
-}
-
-function leftclick (event) {
-	
-	$('a.prizesleft').unbind();
-	$(this).stop();
-	$('a.prizesleft, a.prizesright').css('opacity', '0.7');
-	var l = parseInt ( $('#prizes div a').css('left') );
-	if (l) { // slide right
-		$('#prizes div a').animate ( { 'left': ( l + prizeswidth ) + 'px' }, 200, function () {
-			$('a.prizesleft').click ( leftclick );
-		});
-	}
-	else { // bounce in place
-		$('a.prizesleft').css('opacity', '0.3');
-		$('#prizes div a').animate ( { 'margin-left': '50px' }, 200, function () {
-			$('#prizes div a').animate ( { 'margin-left': '0px' }, 100, function () {
-				$('a.prizesleft').click ( leftclick );
-			});
-		});
-	}
-}
-
-function rightclick (event) {
-	$('a.prizesright').unbind();
-	$(this).stop();
-	$('a.prizesleft, a.prizesright').css('opacity', '0.7');
-	var l = parseInt ( $('#prizes div a').css('left') );
-	if ( (l + $('#prizes div a').width()) > (prizeswidth) ) { // slide left
-		$('#prizes div a').animate ( { 'left': ( l - prizeswidth ) + 'px' }, 200, function () {
-			$('a.prizesright').click ( rightclick );
-		});
-	}
-	else { // bounce in place
-		$('a.prizesright').css('opacity', '0.3');
-		$('#prizes div a').animate ( { 'margin-left': '-50px' }, 200, function () {
-			$('#prizes div a').animate ( { 'margin-left': '0' }, 100, function () {
-				$('a.prizesright').click ( rightclick );
-			});
-		});
-	}
 }
 
 /* Tooltip */
@@ -312,37 +253,6 @@ function scrollTo(to, callback, duration) {
 
 $(document).ready(function() {
 
-/* Prizes slider: Randomize prizes */
-
-	$('body').append("<div id=\"dummy\"></div>");
-	$('#dummy').hide();
-
-	numberprizes = $('#prizes div a img').length;
-
-	var positions = new Array( numberprizes );
-	positions = picks(numberprizes,numberprizes);
-	
-	$('#prizes div a').children('img').each ( function (n) {
-		$('#dummy').append( $(this).parent().children('img:nth-child(' + (positions[n]) + ')').clone() );
-	});
-
-	$('#prizes div a').html('');
-	
-	$('#dummy').children('img').each ( function (n) {
-		$(this).appendTo('#prizes div a');
-	});
-	
-	$('#dummy').html('');
-	
-	$('#prizes div a').css ( 'width', ((numberprizes+1) * 103) + 'px' )
-
-	prizeswidth = $('#prizes div').width();
-
-/* Prizes slider: Bind control event handlers */
-
-	$('a.prizesleft').click ( leftclick );
-	$('a.prizesright').click ( rightclick );
-
 /* Relay URI parameters to links */
 
 	relay_parameters();
@@ -419,10 +329,6 @@ $(document).ready(function() {
 	if (!is_touch_device()) $('.tool').hover ( show_tip, hide_tip );
 
 	$('.tool').on('touchmove', hide_tip);
-	
-/* Retina images replacement */
-
-	$('img.retina').retina({suffix: "@2x"});
 	
 /* Add 'Back to top' button */
 
