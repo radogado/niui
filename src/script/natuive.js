7afb9e052666b26360a34403951ffc2236332e1a
radogado <rado@me.com>
2017-02-19 19:03:09 +0200

    Tooltip, Nav: Proper Hover/Touch support
    For touch, mouse, touch + mouse devices.

--- src/script/natuive.js
+++ src/script/natuive.js
@@ -1364,6 +1364,46 @@ if (q('.overthrow')) {
 	
 }
 
+// Real time touch detection to support devices with both touch and mouse. http://www.javascriptkit.com/dhtmltutors/sticky-hover-issue-solutions.shtml
+
+;(function(){
+
+    var isTouch = false; //var to indicate current input type (is touch versus no touch) 
+    var isTouchTimer;
+    var curRootClass = ''; //var indicating current document root class ("can-touch" or "")
+     
+    function addtouchclass(e) {
+
+        clearTimeout(isTouchTimer);
+        isTouch = true;
+        if (curRootClass != 'can-touch') { //add "can-touch' class if it's not already present
+
+            curRootClass = 'can-touch';
+            document.documentElement.classList.add(curRootClass);
+
+        }
+
+        isTouchTimer = setTimeout(function(){isTouch = false}, 500); //maintain "istouch" state for 500ms so removetouchclass doesn't get fired immediately following a touch event
+
+    }
+     
+    function removetouchclass(e){
+
+        if (!isTouch && curRootClass == 'can-touch'){ //remove 'can-touch' class if not triggered by a touch event and class is present
+
+            isTouch = false;
+            curRootClass = '';
+            document.documentElement.classList.remove('can-touch');
+
+        }
+
+    }
+     
+    document.addEventListener('touchstart', addtouchclass, false); //this event only gets called when input type is touch
+    document.addEventListener('mouseover', removetouchclass, false); //this event gets called when input type is everything from touch to mouse/ trackpad
+
+})();
+
 /* Initialise JS-powered elements */
 
 function init() {
@@ -1566,16 +1606,16 @@ function init() {
 	/* Tooltip */
 	
 	forEach('.tool', function(el, i) {
-	
-/* 		if (touchSupport()) { */
-	
-			el.onclick = function (e) {
+		
+		el.onclick = function (e) {
+
+			if (hasClass(q('html'), 'can-touch')) {
 	
 				toggleClass(eventElement(e), 'open');
-				
-			};
-		
-/* 		} */
+					
+			}
+
+		};		
 	
 	    var t = el.querySelector('.tip');
 	    if (!t) return;
