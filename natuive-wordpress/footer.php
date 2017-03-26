8f17303b56df11ac1210a84056d067d9ec9bcc18
radogado <rado@me.com>
2017-03-26 17:20:50 +0300

    Slider: scrollable overflowing navigation

--- natuive-wordpress/footer.php
+++ natuive-wordpress/footer.php
@@ -5,10 +5,13 @@
 				<div class="row contain">
 					
 				<!-- copyright -->
-					<p class="copyright">
-						&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?>
-						<a href="//wordpress.org" title="WordPress">WordPress</a>, <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a> & <a href="//natuive.net">natUIve WordPress</a>.
-					</p>
+					<div>
+						<p class="copyright">
+							&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?>
+							<a href="//wordpress.org" title="WordPress">WordPress</a>, <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a> & <a href="//natuive.net">natUIve WordPress</a>.
+						</p>
+						<a href=# class="backtotop">🔝</a>
+					</div>
 				<!-- /copyright -->
 				</div>
 
