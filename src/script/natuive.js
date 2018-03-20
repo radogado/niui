b5298f1f6239e15de0fd0e7009dad0b0b953109e
Radoslav Sharapanov <radoslav.sharapanov@paysafe.com>
2018-03-20 16:53:02 +0200

    Grid with inline popups

--- src/script/natuive.js
+++ src/script/natuive.js
@@ -1962,6 +1962,130 @@ function initNav(el) {
 
 }
 
+	// Limitation: each row must have equal width columns.
+	
+function initGridInlinePopups() {
+		
+		forEach(qa('.grid-inline-popup:not([data-ready])'), function (el) {
+		
+		var cells = el.querySelectorAll(':scope > div:not(.popup)');
+		var popups = el.querySelectorAll(':scope > .popup');
+		forEach(cells, function (el) {
+			
+			function openNewItem(e, current_popup) {
+		
+				var cell = e.target.closest('.grid-inline-popup > div');
+				var columns = Math.round(cell.parentElement.scrollWidth/cell.scrollWidth);
+				var el = cell.nextElementSibling;
+				if (el === current_popup) {
+					
+					return;
+
+				}
+				
+				var index = Array.prototype.indexOf.call(popups, el);
+	
+				// Rewind index to start of row (when cell horizontal offest === parent horizontal offset)
+				
+				var index_row = index;
+				
+				while (index_row >= columns && cells[index_row].offsetLeft > cells[index_row].previousElementSibling.previousElementSibling.offsetLeft) {
+					
+					index_row--;
+	
+				}
+	
+				if (index_row < columns) {
+	
+					index_row = 0;
+	
+				}
+	
+				index -= index_row;
+				var i = 0;
+				while(i < index_row) {
+					
+					cells[i++].style.order = -1;
+					
+				}
+				
+				// Set order = -1 until index_row and subtract index_row from index
+				
+				index -= index % columns; // compensate with remainder
+				
+				i = index_row;
+				
+				while(i < (cells.length)) {
+					
+					cells[i].style.order = ((i - index_row) < (index - index_row)) ? -1 : 1;
+					i++;
+					
+				}
+				
+				i = 0;
+				while (i < columns) {
+					
+					cells[i+index_row].style.order = -1;
+					i++;
+					
+				}
+	
+				el.setAttribute('aria-expanded', 'true');
+				var height = el.scrollHeight;
+				el.style.maxHeight = 0;
+				el.style.overflow = 'hidden';
+				animate(el, '100% { max-height: ' + height + 'px; }', .2, function () {
+		
+					el.setAttribute('aria-expanded', 'true');
+					el.style.cssText = '';
+					
+				});
+	
+			}
+					
+			function openCell(e) {
+				
+				var current_popup = closest(e.target, '.grid-inline-popup').querySelector('.popup[aria-expanded]');
+				if (current_popup) {
+					
+					current_popup.style.maxHeight = current_popup.scrollHeight + 'px';
+					current_popup.style.overflow = 'hidden';
+					animate(current_popup, '100% { max-height: 0; }', .2, function () {
+						
+						current_popup.removeAttribute('aria-expanded');
+						current_popup.style.cssText = '';
+						openNewItem(e, current_popup);
+				
+					});
+					
+				} else {
+				
+					openNewItem(e);
+				
+				}
+				
+			}
+	
+			el.setAttribute('tabindex', 0);
+			el.addEventListener('click', openCell);
+			el.addEventListener('touchend', openCell);
+			el.addEventListener('keyup', function (e) { 
+				
+				if (e.key === 'Enter') {
+	
+					openCell(e);
+	
+				}
+			
+			});
+			
+		});
+		el.setAttribute('data-ready', true);
+		
+	});
+	
+	}
+
 var current_slider = null;
 
 /* Initialise JS-powered elements */
@@ -2214,6 +2338,8 @@ function init() {
 		initNav(el);
 		
 	});
+
+	initGridInlinePopups();
 
 }
 
