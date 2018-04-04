a85d04042dc7dfd2f488057d8a06329413c313cb
Radoslav Sharapanov <radoslav.sharapanov@paysafe.com>
2018-04-04 10:30:41 +0300

    Slider: padding and peeking support

---
+++ demos/enoden/enoden_files/cse.js
@@ -0,0 +1,38 @@
+(function(opts_){window.__gcse=window.__gcse||{};window.__gcse.ct=(new Date).getTime();window.__gcse.scb=function(){var a=window.__gcse;a.plainStyle&&delete opts_.rawCss;google.search.cse.element.init(opts_)&&("explicit"!=a.parsetags?"complete"==document.readyState||"interactive"==document.readyState?(google.search.cse.element.go(),a.callback&&a.callback()):google.setOnLoadCallback(function(){google.search.cse.element.go();a.callback&&a.callback()},!0):a.callback&&a.callback())};
+var b=document.createElement("script"),c=opts_.protocol+"://"+opts_.uds+"/"+(opts_.loaderPath||"jsapi")+"?autoload=",d=encodeURIComponent,e='{"name":"search","version":"1.0","callback":"__gcse.scb"',f=window.__gcse;
+if(!f||!f.plainStyle){var g=opts_.protocol+(opts_.uiOptions&&opts_.uiOptions.enableMobileLayout?"://www.google.com/cse/static/style/look/mobile/":"://www.google.com/cse/static/style/look/"),h,k=opts_.theme,l=opts_.uiOptions.forceV2LookAndFeel;k=k.toLowerCase();h=l?"v2/"+k.replace("v2_",""):k.replace("v2_","v2/");e+=',"style":"'+(g+h+".css")+'"'}opts_.language&&(e+=',"language":"'+opts_.language+'"');var m=c+d('{"modules":['+(e+"}")+"]}");opts_.callback&&(m=m+"&callback="+opts_.callback);b.src=m+"";
+b.type="text/javascript";document.getElementsByTagName("head")[0].appendChild(b);
+})({
+  "cx": "008531154011676020290:wehwoa3i740",
+  "language": "ja",
+  "theme": "V2_DEFAULT",
+  "uiOptions": {
+    "resultsUrl": "",
+    "enableAutoComplete": true,
+    "enableImageSearch": false,
+    "imageSearchLayout": "popup",
+    "resultSetSize": "filtered_cse",
+    "gaQueryParameter": "q",
+    "gaCategoryParameter": "",
+    "enableOrderBy": true,
+    "orderByOptions": [{
+      "label": "Relevance",
+      "key": ""
+    }, {
+      "label": "Date",
+      "key": "date"
+    }],
+    "overlayResults": true,
+    "enableMobileLayout": false,
+    "numTopRefinements": -1,
+    "isSitesearch": false,
+    "enableSpeech": true,
+    "forceV2LookAndFeel": false
+  },
+  "protocol": "https",
+  "uds": "www.google.com",
+  "rawCss": ".gsc-control-cse{font-family:Arial, sans-serif;border-color:#666666;background-color:#FFFFFF}.gsc-control-cse .gsc-table-result{font-family:Arial, sans-serif}input.gsc-input,.gsc-input-box,.gsc-input-box-hover,.gsc-input-box-focus{border-color:#D9D9D9}.gsc-search-button-v2,.gsc-search-button-v2:hover,.gsc-search-button-v2:focus{border-color:#2F5BB7;background-color:#357AE8;background-image:none;filter:none}.gsc-search-button-v2 svg{fill:#FFFFFF}.gsc-tabHeader.gsc-tabhInactive{border-color:#CCCCCC;background-color:#FFFFFF}.gsc-tabHeader.gsc-tabhActive{border-color:#CCCCCC;border-bottom-color:#FFFFFF;background-color:#FFFFFF}.gsc-tabsArea{border-color:#CCCCCC}.gsc-webResult.gsc-result,.gsc-results .gsc-imageResult{border-color:#FFFFFF;background-color:#FFFFFF}.gsc-webResult.gsc-result:hover,.gsc-imageResult:hover{border-color:#FFFFFF;background-color:#FFFFFF}.gs-webResult.gs-result a.gs-title:link,.gs-webResult.gs-result a.gs-title:link b,.gs-imageResult a.gs-title:link,.gs-imageResult a.gs-title:link b{color:#1155CC}.gs-webResult.gs-result a.gs-title:visited,.gs-webResult.gs-result a.gs-title:visited b,.gs-imageResult a.gs-title:visited,.gs-imageResult a.gs-title:visited b{color:#1155CC}.gs-webResult.gs-result a.gs-title:hover,.gs-webResult.gs-result a.gs-title:hover b,.gs-imageResult a.gs-title:hover,.gs-imageResult a.gs-title:hover b{color:#1155CC}.gs-webResult.gs-result a.gs-title:active,.gs-webResult.gs-result a.gs-title:active b,.gs-imageResult a.gs-title:active,.gs-imageResult a.gs-title:active b{color:#1155CC}.gsc-cursor-page{color:#1155CC}a.gsc-trailing-more-results:link{color:#1155CC}.gs-webResult .gs-snippet,.gs-imageResult .gs-snippet,.gs-fileFormatType{color:#333333}.gs-webResult div.gs-visibleUrl,.gs-imageResult div.gs-visibleUrl{color:#009933}.gs-webResult div.gs-visibleUrl-short{color:#009933}.gs-webResult div.gs-visibleUrl-short{display:none}.gs-webResult div.gs-visibleUrl-long{display:block}.gs-promotion div.gs-visibleUrl-short{display:none}.gs-promotion div.gs-visibleUrl-long{display:block}.gsc-cursor-box{border-color:#FFFFFF}.gsc-results .gsc-cursor-box .gsc-cursor-page{border-color:#CCCCCC;background-color:#FFFFFF;color:#1155CC}.gsc-results .gsc-cursor-box .gsc-cursor-current-page{border-color:#CCCCCC;background-color:#FFFFFF;color:#1155CC}.gsc-webResult.gsc-result.gsc-promotion{border-color:#F6F6F6;background-color:#F6F6F6}.gsc-completion-title{color:#1155CC}.gsc-completion-snippet{color:#333333}.gs-promotion a.gs-title:link,.gs-promotion a.gs-title:link *,.gs-promotion .gs-snippet a:link{color:#1155CC}.gs-promotion a.gs-title:visited,.gs-promotion a.gs-title:visited *,.gs-promotion .gs-snippet a:visited{color:#1155CC}.gs-promotion a.gs-title:hover,.gs-promotion a.gs-title:hover *,.gs-promotion .gs-snippet a:hover{color:#1155CC}.gs-promotion a.gs-title:active,.gs-promotion a.gs-title:active *,.gs-promotion .gs-snippet a:active{color:#1155CC}.gs-promotion .gs-snippet,.gs-promotion .gs-title .gs-promotion-title-right,.gs-promotion .gs-title .gs-promotion-title-right *{color:#333333}.gs-promotion .gs-visibleUrl,.gs-promotion .gs-visibleUrl-short{color:#009933}",
+  "loaderPath": "jsapi",
+  "cse_token": "ABPF6Hj3dZgpVoRNSTIelEnjfi5LrzAYYQ:1522786619045",
+  "isHostedPage": false
+});
