42f84fe0f4c627cff6079f2f23012d33b38f9fa2
Radoslav Sharapanov <radoslav.sharapanov@paysafe.com>
2019-03-25 14:16:29 +0200

    Default dist back to 10K CSS with natuive-extra.min.* for all components

--- src/script/natuive-extra.js
+++ src/script/natuive-extra.js
@@ -3411,4 +3411,209 @@ var componentSlider = (function (){
 })();
 
 // Component Fold – end
-initComponents(); return { initComponents, animate, copyButton, componentModal, addComponent, componentSlider } })()
+;// Component Grid with inline popups – start
+
+(function (){
+    
+/* Grid with inline popups – start */
+
+function initGridInlinePopups(host) { // Limitation: each row must have equal width columns.
+		
+	host.querySelectorAll('.grid-inline-popup:not([data-ready])').forEach((el) => {
+		
+		var id = `id${new Date().getTime()}`; // Unique id
+		el.id = el.id || id;
+		var cells = el.querySelectorAll(`#${el.id} > div:not(.grid-inline-popup--popup)`);
+		var popups = el.querySelectorAll(`#${el.id} > .grid-inline-popup--popup`);
+		
+		if (el.id === id) {
+			
+			el.removeAttribute('id');
+
+		}
+		
+		cells.forEach((el) => {
+			
+			function openNewItem(e, current_popup) {
+		
+				var cell = e.target.closest('.grid-inline-popup > div');
+				var columns = Math.round(cell.parentElement.scrollWidth/cell.scrollWidth);
+				var el = cell.nextElementSibling;
+				if (el === current_popup) {
+					
+					return;
+
+				}
+				
+				var index = Array.prototype.indexOf.call(popups, el);
+	
+				// Rewind index to start of row (when cell horizontal offest === parent horizontal offset)
+				
+				var index_row = index;
+				
+				while (index_row >= columns && cells[index_row].offsetLeft > cells[index_row].previousElementSibling.previousElementSibling.offsetLeft) {
+					
+					index_row--;
+	
+				}
+	
+				if (index_row < columns) {
+	
+					index_row = 0;
+	
+				}
+	
+				index -= index_row;
+				var i = 0;
+				while(i < index_row) {
+					
+					cells[i++].style.order = -1;
+					
+				}
+				
+				// Set order = -1 until index_row and subtract index_row from index
+				
+				index -= index % columns; // compensate with remainder
+				
+				i = index_row;
+				
+				while(i < (cells.length)) {
+					
+					cells[i].style.order = ((i - index_row) < (index - index_row)) ? -1 : 1;
+					i++;
+					
+				}
+				
+				i = 0;
+				while (i < columns) {
+					
+					cells[i+index_row].style.order = -1;
+					i++;
+					
+				}
+	
+				el.setAttribute(aria_expanded, 'true');
+				el.previousElementSibling.setAttribute(aria_expanded, 'true');
+				var height = el.scrollHeight;
+				el.style.maxHeight = 0;
+				el.style.overflow = 'hidden';
+				animate(el, `100% { max-height: ${height}px; }`, .2, () => {
+		
+					el.style.cssText = '';
+					
+				});
+	
+			}
+					
+			function openCell(e) {
+				
+				var current_popup = e.target.closest('.grid-inline-popup').querySelector('.grid-inline-popup--popup[aria-expanded]');
+				if (current_popup) {
+					
+					current_popup.style.maxHeight = current_popup.scrollHeight + 'px';
+					current_popup.style.overflow = 'hidden';
+					animate(current_popup, '100% { max-height: 0; }', .2, () => {
+						
+						current_popup.removeAttribute(aria_expanded);
+						current_popup.previousElementSibling.removeAttribute(aria_expanded);
+						current_popup.style.cssText = '';
+						openNewItem(e, current_popup);
+				
+					});
+					
+				} else {
+				
+					openNewItem(e);
+				
+				}
+				
+			}
+	
+			el.setAttribute('tabindex', 0);
+			el.addEventListener('click', openCell);
+			el.addEventListener('touchend', openCell);
+			el.addEventListener('keyup', (e) => { 
+				
+				if (e.key === 'Enter') {
+	
+					openCell(e);
+	
+				}
+			
+			});
+			
+		});
+		makeReady(el);
+		
+	});
+	
+}
+
+/* Grid with inline popups – end */
+
+	var init = (host) => {
+
+		initGridInlinePopups(host);
+		
+	};
+	registerComponent('grid-inline-popups', init);
+
+})();
+
+// Component Grid with inline popups – end
+;// Component Notification bar – start
+
+var componentNotify = (function (){
+    
+	function notifyClose(el) {
+		
+		el.parentNode.removeChild(el);
+		
+	}
+	
+	function notifyCloseEvent() {
+	
+		if (q('.n-notify')) {
+	
+			q('.n-notify').onclick = (e) => {
+				
+				notifyClose(e.target);	
+					
+			};
+		
+		}
+		
+	}
+	
+	function notify(content, option) {
+		
+		bodyElement.insertAdjacentHTML('afterbegin', `<div class="n-notify${(option && (option.indexOf('fixed') !== -1) ? ' n-notify--fixed' : '')}">${content}</div>`);
+		notifyCloseEvent();
+		if (option && option.indexOf('timeout') !== -1) {
+			
+			setTimeout(() => { notifyClose(q('.n-notify')) }, 2000);
+	
+		}
+		
+	}
+
+	var init = (host) => {
+		
+		/* Tooltip */
+		
+		host.querySelectorAll('.n-notify:not([data-ready])').forEach((el, i) => {
+			
+			notifyCloseEvent();
+			makeReady(el);
+		
+		});
+		
+	};
+	registerComponent('notify', init);
+
+	return { notify: notify };
+
+})();
+
+// Component Notification bar – end
+initComponents(); return { initComponents, animate, copyButton, componentNotify, componentModal, addComponent, componentSlider } })()
