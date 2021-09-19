30c42ba50060df99c34d99b2e1fc143fc8d22130
Radoslav Sharapanov <rado@me.com>
2021-09-19 17:36:52 +0300

    ,

---
+++ src.dev/components/n-popin/popin.js
@@ -0,0 +1,26 @@
+// Component Popin – start
+
+(function() {
+
+	let init = (host) => {
+
+		host.querySelectorAll('.n-popin > * > button').forEach(el => {
+			el.onclick = e => {
+				let el = e.target;
+				let container = el.closest('.n-popin');
+				let content = el.parentNode;
+				let previous = container.querySelector(':scope > [aria-expanded]');
+				previous?.removeAttribute('aria-expanded');
+				if (previous !== content) {
+					let row = Math.floor([...el.parentNode.parentNode.children].indexOf(el.parentNode) / getComputedStyle(container).getPropertyValue('--n-popin-columns') * 1) + 2;
+					container.style.setProperty('--n-popin-open-row', row);
+					content.ariaExpanded = true;
+				}
+			}
+		});
+
+	};
+	registerComponent("popin", init);
+})();
+
+// Component Popin – end
