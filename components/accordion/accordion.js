27b6493a6800962ad997bb8696dd8e23c7cbc988
Radoslav Sharapanov <rado@me.com>
2022-05-23 08:59:35 +0300

    ,

--- components/accordion/accordion.js
+++ components/accordion/accordion.js
@@ -6,6 +6,7 @@
     window.requestAnimationFrame(() => {
       el.style.height = 0;
       el.style.overflow = "hidden";
+      el.parentNode.setAttribute("aria-expanded", true);
       el.animate([{ height: 0 }, { height: `${el.scrollHeight}px` }], animate_options).onfinish = () => {
         el.style.height = el.style.overflow = "";
       };
@@ -14,11 +15,11 @@
 
   const closeAccordion = (el) => {
     window.requestAnimationFrame(() => {
-      el.parentNode.open = true;
+      // el.parentNode.open = true;
       el.style.overflow = "hidden";
       el.animate([{ height: `${el.scrollHeight}px` }, { height: 0 }], animate_options).onfinish = () => {
         el.style.height = el.style.overflow = "";
-        el.parentNode.removeAttribute("open");
+        el.parentNode.removeAttribute("aria-expanded");
       };
     });
   };
@@ -26,16 +27,16 @@
   const toggleAccordion = (e) => {
     let el = e.target.parentNode;
 
-    if (!el.open) {
+    if (!el.getAttribute('aria-expanded')) {
       openAccordion(e.target.nextElementSibling);
     } else {
       closeAccordion(e.target.nextElementSibling);
     }
     let container = el.closest(".n-accordion__popin");
     if (el.parentNode.classList.contains("n-accordion__group") || container) {
-      el.parentNode.querySelectorAll(":scope > details[open]").forEach((el2) => {
+      el.parentNode.querySelectorAll(":scope > .n-accordion[aria-expanded]").forEach((el2) => {
         if (el2 !== el) {
-          closeAccordion(el2.querySelector(":scope > :not(summary)"));
+          closeAccordion(el2.querySelector(":scope > .n-accordion__content"));
         }
       });
     }
@@ -46,8 +47,9 @@
   };
 
   function init(host) {
-    host.querySelectorAll(".n-accordion:not([data-ready]) > summary").forEach((el) => {
+    host.querySelectorAll(".n-accordion:not([data-ready]) > .n-accordion__label").forEach((el) => {
       el.addEventListener("click", toggleAccordion);
+      el.parentElement.querySelector(":scope > input")?.remove(); // Remove CSS-only solution
       el.parentNode.dataset.ready = true;
     });
   }
