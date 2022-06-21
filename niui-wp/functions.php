2acd73478c18291b0ae430175eb0b98dd8ecc4bb
Rado <spacest@gmail.com>
2022-06-21 11:25:11 +0300

    ,

--- niui-wp/functions.php
+++ niui-wp/functions.php
@@ -695,8 +695,9 @@ add_action('the_content', function ($content) {
 		}
 		
 		$wrapper = $dom->createElement('span');
+    $image_id = preg_replace('/[^0-9]/', '', $img->getAttribute('class'));
 		$wrapper->setAttribute('class', 'n-aspect ' . $img->getAttribute('class'));
-		$wrapper->setAttribute('style', '--width: ' . $img->getAttribute('width') . '; --height: ' . $img->getAttribute('height'));
+    $wrapper->setAttribute('style', '--width: ' . $img->getAttribute('width') . '; --height: ' . $img->getAttribute('height') . '; background-image: url(' . wp_get_attachment_image_url($image_id, 'medium') . ');');
 		
 		$img->parentNode->replaceChild($wrapper, $img);
 
