0f2b1ea681f457ec3101756d81fb865d102cd4be
Radoslav Sharapanov <rado@me.com>
2023-04-01 00:13:00 +0300

    ,

--- js/niui-tail.js
+++ js/niui-tail.js
@@ -1,3 +1,5 @@
-initComponents(); return { initComponents, copyButton, componentModal.openModal: modal, addComponent, componentNotify } })();
-
+initComponents();
+const modal = componentModal.openModal;
+return { initComponents, copyButton, modal, addComponent, componentNotify }
+})();
 window.nui.dynamicInit = true;
