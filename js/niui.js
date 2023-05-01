aa1fc469c980d18f2934e6f66dcc7446a76ba365
Radoslav Sharapanov <rado@me.com>
2023-05-01 23:00:01 +0300

    3.0

--- js/niui.js
+++ js/niui.js
@@ -481,6 +481,85 @@ nui.dynamicInit = true;// Component Button – start
 // Component Form – end
 //# sourceMappingURL=form.js.map
 
+// Component Accordion
+(function() {
+	const animate_options = el => { return { easing: "ease-in-out", duration: window.matchMedia("(prefers-reduced-motion: no-preference)").matches ? (el.dataset.duration * 1000 || getComputedStyle(el).getPropertyValue('--duration') * 1000 || 200) : 0 } };
+	const accordionContent = el => el.querySelector(":scope > .n-accordion__content");
+	const openAccordion = (el) => {
+		el = accordionContent(el);
+		window.requestAnimationFrame(() => {
+			el.style.height = 0;
+			el.style.overflow = "hidden";
+			let wrapper = el.parentNode;
+			wrapper.querySelector(":scope > .n-accordion__label").setAttribute("aria-expanded", true);
+			el.animate([{ height: 0 }, { height: `${el.scrollHeight}px` }], animate_options(wrapper)).onfinish = () => {
+				el.style.height = el.style.overflow = "";
+			};
+		});
+	};
+	const closeAccordion = (el, callback) => {
+		el = accordionContent(el);
+		window.requestAnimationFrame(() => {
+			el.style.overflow = "hidden";
+			let wrapper = el.parentNode;
+			el.animate([{ height: `${el.scrollHeight}px` }, { height: 0 }], animate_options(wrapper)).onfinish = () => {
+				el.style.height = el.style.overflow = "";
+				wrapper.querySelector(":scope > .n-accordion__label").setAttribute("aria-expanded", false);
+				typeof callback !== 'function' || callback();
+				if (wrapper.classList.contains('n-accordion--close-nested')) {
+					el.querySelectorAll(".n-accordion__label[aria-expanded='true']").forEach(el => el.setAttribute("aria-expanded", false));
+				}
+			};
+		});
+	};
+	const toggleAccordion = (e) => {
+		let el = e.target.closest('.n-accordion'); // el = .n-accordion
+		if (!el.querySelector(":scope > [aria-expanded='true']")) {
+			let popin = el.closest(".n-accordion__popin");
+			const updateRow = () => {
+				if (popin) {
+					let row = Math.floor(([...popin.children].indexOf(el) / getComputedStyle(popin).getPropertyValue("--n-popin-columns")) * 1) + 2;
+					popin.style.setProperty("--n-popin-open-row", row);
+				}
+			};
+			if (el.parentNode.matches('[role="group"]') || popin) {
+				let other_accordion = el.parentNode.querySelector(":scope > .n-accordion > [aria-expanded='true']");
+				if (other_accordion) {
+					closeAccordion(other_accordion.parentNode, () => { // el = .n-accordion
+						updateRow();
+						openAccordion(el);
+					});
+				} else {
+					updateRow();
+					openAccordion(el);
+				}
+			} else {
+				openAccordion(el);
+			}
+		} else {
+			closeAccordion(el);
+		}
+	};
+
+	function init(host = document) {
+		host.querySelectorAll(".n-accordion:not([data-ready]) > .n-accordion__label").forEach((el) => {
+			el.addEventListener("click", toggleAccordion);
+			el.parentElement.querySelector(":scope > input")?.remove(); // Remove CSS-only solution
+			el.parentNode.dataset.ready = true;
+			el.setAttribute('aria-expanded', el.getAttribute('aria-expanded') === 'true');
+		});
+	}
+	const doInit = () => {
+		(typeof nui !== 'undefined' && typeof nui.registerComponent === "function") ? nui.registerComponent("n-accordion", init) : init();
+	};
+	if (document.readyState !== "loading") {
+		doInit();
+	} else {
+		document.addEventListener("DOMContentLoaded", doInit);
+	}
+})();
+//# sourceMappingURL=n-accordion@npm.js.map
+
 // import './node_modules/n-modal/n-modal.js';
 (function() {
   const ceilingWidth = (el) => Math.ceil(parseFloat(getComputedStyle(el).width));
@@ -1082,15 +1161,24 @@ nui.dynamicInit = true;// Component Button – start
       trapFocus(carousel.closest(".n-carousel"));
     }
   };
-  const verticalAutoObserver = new ResizeObserver((entries) => {
+  const autoHeightObserver = new ResizeObserver((entries) => {
     window.requestAnimationFrame(() => {
       entries.forEach((e) => {
-        let slide = e.target.closest(".n-carousel__content > *");
+        let slide = e.target.querySelector(":scope > [aria-current]");
         let el = slide.closest(".n-carousel__content");
-        if (!!slide.parentNode.ariaCurrent && !el.parentNode.dataset.sliding) {
-          slide.style.height = 'auto';
-          el.style.height = `${slide.scrollHeight}px`;
-          slide.style.height = '';
+        if (!el.parentElement.dataset.sliding) {
+          // console.log(e.target);
+          el.parentNode.style.removeProperty('--height');
+          if (isVertical(el)) {
+            slide.style.height = 'auto';
+            el.style.height = `${slide.scrollHeight}px`;
+            slide.style.height = '';
+            updateCarousel(el);
+          } else {
+            el.style.height = '';
+            el.style.height = `${slide.scrollHeight}px`;
+            updateCarousel(el, true);
+          }
         }
       });
     });
@@ -1291,9 +1379,9 @@ nui.dynamicInit = true;// Component Button – start
         el.dataset.ready = true;
         content.scrollTop = 0; // Should be a different value if the initial active slide is other than the first one (unless updateCarousel() takes care of it)
       }
-      if (el.matches(".n-carousel--vertical.n-carousel--auto-height")) {
-        // Vertical auto has a specified height which needs update on resize
-        content.querySelectorAll(":scope > * > *").forEach((el) => verticalAutoObserver.observe(el));
+      if (el.matches(".n-carousel--auto-height")) {
+        // Auto has a specified height which needs update on resize
+        autoHeightObserver.observe(content);
       }
       window.requestAnimationFrame(() => {
         observersOn(content);
@@ -1725,85 +1813,6 @@ nui.dynamicInit = true;// Component Button – start
 /* Modal – end */
 //# sourceMappingURL=n-modal@npm.js.map
 
-// Component Accordion
-(function() {
-	const animate_options = el => { return { easing: "ease-in-out", duration: window.matchMedia("(prefers-reduced-motion: no-preference)").matches ? (el.dataset.duration * 1000 || getComputedStyle(el).getPropertyValue('--duration') * 1000 || 200) : 0 } };
-	const accordionContent = el => el.querySelector(":scope > .n-accordion__content");
-	const openAccordion = (el) => {
-		el = accordionContent(el);
-		window.requestAnimationFrame(() => {
-			el.style.height = 0;
-			el.style.overflow = "hidden";
-			let wrapper = el.parentNode;
-			wrapper.querySelector(":scope > .n-accordion__label").setAttribute("aria-expanded", true);
-			el.animate([{ height: 0 }, { height: `${el.scrollHeight}px` }], animate_options(wrapper)).onfinish = () => {
-				el.style.height = el.style.overflow = "";
-			};
-		});
-	};
-	const closeAccordion = (el, callback) => {
-		el = accordionContent(el);
-		window.requestAnimationFrame(() => {
-			el.style.overflow = "hidden";
-			let wrapper = el.parentNode;
-			el.animate([{ height: `${el.scrollHeight}px` }, { height: 0 }], animate_options(wrapper)).onfinish = () => {
-				el.style.height = el.style.overflow = "";
-				wrapper.querySelector(":scope > .n-accordion__label").setAttribute("aria-expanded", false);
-				typeof callback !== 'function' || callback();
-				if (wrapper.classList.contains('n-accordion--close-nested')) {
-					el.querySelectorAll(".n-accordion__label[aria-expanded='true']").forEach(el => el.setAttribute("aria-expanded", false));
-				}
-			};
-		});
-	};
-	const toggleAccordion = (e) => {
-		let el = e.target.closest('.n-accordion'); // el = .n-accordion
-		if (!el.querySelector(":scope > [aria-expanded='true']")) {
-			let popin = el.closest(".n-accordion__popin");
-			const updateRow = () => {
-				if (popin) {
-					let row = Math.floor(([...popin.children].indexOf(el) / getComputedStyle(popin).getPropertyValue("--n-popin-columns")) * 1) + 2;
-					popin.style.setProperty("--n-popin-open-row", row);
-				}
-			};
-			if (el.parentNode.matches('[role="group"]') || popin) {
-				let other_accordion = el.parentNode.querySelector(":scope > .n-accordion > [aria-expanded='true']");
-				if (other_accordion) {
-					closeAccordion(other_accordion.parentNode, () => { // el = .n-accordion
-						updateRow();
-						openAccordion(el);
-					});
-				} else {
-					updateRow();
-					openAccordion(el);
-				}
-			} else {
-				openAccordion(el);
-			}
-		} else {
-			closeAccordion(el);
-		}
-	};
-
-	function init(host = document) {
-		host.querySelectorAll(".n-accordion:not([data-ready]) > .n-accordion__label").forEach((el) => {
-			el.addEventListener("click", toggleAccordion);
-			el.parentElement.querySelector(":scope > input")?.remove(); // Remove CSS-only solution
-			el.parentNode.dataset.ready = true;
-			el.setAttribute('aria-expanded', el.getAttribute('aria-expanded') === 'true');
-		});
-	}
-	const doInit = () => {
-		(typeof nui !== 'undefined' && typeof nui.registerComponent === "function") ? nui.registerComponent("n-accordion", init) : init();
-	};
-	if (document.readyState !== "loading") {
-		doInit();
-	} else {
-		document.addEventListener("DOMContentLoaded", doInit);
-	}
-})();
-//# sourceMappingURL=n-accordion@npm.js.map
-
 (function() {
 	const isChrome = !!navigator.userAgent.match("Chrome");
 	navigator.userAgent.match(/Safari/) && !isChrome;
@@ -2196,47 +2205,6 @@ nui.dynamicInit = true;// Component Button – start
 })();
 //# sourceMappingURL=n-select@npm.js.map
 
-// Component Notification bar – start
-(function() {
-	function notifyClose(el) {
-		if (!!el) {
-			el.parentNode.removeChild(el);
-		}
-	}
-
-	function notifyCloseEvent() {
-		if (document.querySelector(".n-notify")) {
-			document.querySelector(".n-notify").onclick = (e) => {
-				notifyClose(e.target);
-			};
-		}
-	}
-
-	function notify(content, option) {
-		document.body.insertAdjacentHTML("afterbegin", `<button class="n-notify${option && option.indexOf("fixed") !== -1 ? " n-notify--fixed" : ""}">${content}</button>`);
-		document.querySelector(".n-notify").focus();
-		notifyCloseEvent();
-		if (option && option.indexOf("timeout") !== -1) {
-			setTimeout(() => {
-				notifyClose(document.querySelector(".n-notify"));
-			}, 2000);
-		}
-	}
-	let init = (host) => {
-		/* Tooltip */
-		host.querySelectorAll(".n-notify:not([data-ready])").forEach((el, i) => {
-			notifyCloseEvent();
-			el.dataset.ready = true;
-		});
-	};
-	nui.registerComponent("notify", init, {
-		'name': 'notify',
-		'code': notify
-	});
-})();
-// Component Notification bar – end
-//# sourceMappingURL=notify.js.map
-
 // Component Tooltip – start
 (function() {
 	let setTipPosition = (tool, tip) => {
@@ -2627,6 +2595,47 @@ nui.dynamicInit = true;// Component Button – start
 })();
 // Component Nav – end
 //# sourceMappingURL=nav.js.map
+
+// Component Notification bar – start
+(function() {
+	function notifyClose(el) {
+		if (!!el) {
+			el.parentNode.removeChild(el);
+		}
+	}
+
+	function notifyCloseEvent() {
+		if (document.querySelector(".n-notify")) {
+			document.querySelector(".n-notify").onclick = (e) => {
+				notifyClose(e.target);
+			};
+		}
+	}
+
+	function notify(content, option) {
+		document.body.insertAdjacentHTML("afterbegin", `<button class="n-notify${option && option.indexOf("fixed") !== -1 ? " n-notify--fixed" : ""}">${content}</button>`);
+		document.querySelector(".n-notify").focus();
+		notifyCloseEvent();
+		if (option && option.indexOf("timeout") !== -1) {
+			setTimeout(() => {
+				notifyClose(document.querySelector(".n-notify"));
+			}, 2000);
+		}
+	}
+	let init = (host) => {
+		/* Tooltip */
+		host.querySelectorAll(".n-notify:not([data-ready])").forEach((el, i) => {
+			notifyCloseEvent();
+			el.dataset.ready = true;
+		});
+	};
+	nui.registerComponent("notify", init, {
+		'name': 'notify',
+		'code': notify
+	});
+})();
+// Component Notification bar – end
+//# sourceMappingURL=notify.js.map
 
 // Component Parallax – start
 (function() {
